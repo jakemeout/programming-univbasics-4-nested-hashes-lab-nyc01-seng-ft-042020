c41002906bac3a2bc12e333e6feabee06610b076
Jacob Hyde <jacobosity@gmail.com>
2020-03-28 20:52:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:known_for]["Unix"][0]
+programmer_hash[:dennis_ritchie][:known_for]["Unix"][:languages][0]
 end
 
 def adding_matz
